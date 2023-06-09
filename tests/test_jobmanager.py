53c88a7f97e7bb328f93986a02204e3db03fef72
Lukas Januschke <st161630@stud.uni-stuttgart.de>
2023-06-09 18:46:39 +0200

    Update json filename and loading

--- tests/test_jobmanager.py
+++ tests/test_jobmanager.py
@@ -1,14 +1,35 @@
+# Copyright 2023 University of Stuttgart
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import pathlib
+
 import pytest
 from qunicorn_core.api import *
 from qunicorn_core.api.jobmanager.jobs import createJob, JobRegister
 from collections import namedtuple
 import json
 
+""""Test class to test the functionality of the jobmanager"""
+
+
 def test_create_job():
-    # Make sure to add a valid token to the test_data.json
-    f = open("test_data.json")
-    new_job_data = json.load(f)
-    job: JobRegister = namedtuple("JobRegister", new_job_data.keys())(*new_job_data.values())
+    """"
+    Tests the create job method.
+    """
+    # Make sure to add a valid token to the jobmanager_test_data.json
+    with open('tests/jobmanager_test_data.json') as f:
+        data = json.load(f)
+    job: JobRegister = namedtuple("JobRegister", data.keys())(*data.values())
     result = createJob(job)
 
     # Check if Counts are within certain range
