bcc808f3cde99e1258b5d5648c9718b70ab3d769
ArneBartenbach <44618971+ArneBartenbach@users.noreply.github.com>
2023-08-13 19:44:00 +0200

    rename qiskit pilot

--- qunicorn_core/core/pilotmanager/ibm_pilot.py
+++ qunicorn_core/core/pilotmanager/ibm_pilot.py
@@ -38,11 +38,11 @@ from qunicorn_core.static.enums.result_type import ResultType
 from qunicorn_core.util import logging
 
 
-class QiskitPilot(Pilot):
-    """The Qiskit Pilot"""
+class IBMPilot(Pilot):
+    """The IBM Pilot"""
 
     def execute(self, job_core_dto: JobCoreDto):
-        """Execute a job on an IBM backend using the Qiskit Pilot"""
+        """Execute a job on an IBM backend using the IBM Pilot"""
 
         if job_core_dto.type == JobType.RUNNER:
             if job_core_dto.executed_on.device_name == "aer_simulator":
@@ -81,7 +81,7 @@ class QiskitPilot(Pilot):
         logging.info(f"Run job with id {job_dto.id} locally on aer_simulator and get the result {results}")
 
     def __run(self, job_dto: JobCoreDto):
-        """Run a job on an IBM backend using the Qiskit Pilot"""
+        """Run a job on an IBM backend using the IBM Pilot"""
         provider = self.__get_ibm_provider_login_and_update_job(job_dto.token, job_dto.id)
         backend, transpiled = self.transpile(provider, job_dto)
         job_db_service.update_attribute(job_dto.id, JobState.RUNNING, JobDataclass.state)
@@ -95,7 +95,7 @@ class QiskitPilot(Pilot):
         )
 
     def __sample(self, job_dto: JobCoreDto):
-        """Uses the Sampler to execute a job on an IBM backend using the Qiskit Pilot"""
+        """Uses the Sampler to execute a job on an IBM backend using the IBM Pilot"""
         backend, circuits = self.__get_backend_circuits_and_id_for_qiskit_runtime(job_dto)
         sampler = Sampler(session=backend)
 
@@ -108,7 +108,7 @@ class QiskitPilot(Pilot):
         )
 
     def __estimate(self, job_dto: JobCoreDto):
-        """Uses the Estimator to execute a job on an IBM backend using the Qiskit Pilot"""
+        """Uses the Estimator to execute a job on an IBM backend using the IBM Pilot"""
         backend, circuits = self.__get_backend_circuits_and_id_for_qiskit_runtime(job_dto)
         estimator = Estimator(session=backend)
         estimator_observables: list[SparsePauliOp] = [SparsePauliOp("IY"), SparsePauliOp("IY")]
@@ -126,7 +126,7 @@ class QiskitPilot(Pilot):
         self.__get_ibm_provider_login_and_update_job(job_dto.token, job_dto.id)
         service: QiskitRuntimeService = QiskitRuntimeService()
         job_db_service.update_attribute(job_dto.id, JobState.RUNNING, JobDataclass.state)
-        circuits: List[QuantumCircuit] = QiskitPilot.__get_circuits_as_QuantumCircuits(job_dto)
+        circuits: List[QuantumCircuit] = IBMPilot.__get_circuits_as_QuantumCircuits(job_dto)
         backend: BackendV1 = service.get_backend(job_dto.executed_on.device_name)
         return backend, circuits
 
@@ -166,7 +166,7 @@ class QiskitPilot(Pilot):
     def __get_ibm_provider_login_and_update_job(token: str, job_dto_id: int) -> IBMProvider:
         """Save account credentials, get provider and update job_dto to job_state = Error, if it is not possible"""
         try:
-            return QiskitPilot.get_ibm_provider_and_login(token)
+            return IBMPilot.get_ibm_provider_and_login(token)
         except Exception as exception:
             job_db_service.update_finished_job(job_dto_id, result_mapper.get_error_results(exception), JobState.ERROR)
             raise exception
