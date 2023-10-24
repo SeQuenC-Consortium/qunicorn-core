ae927a1725cc4335eac47ea5cf0a7ac3d742bfed
BonnetGa <m.erkur@gmx.de>
2023-10-24 16:49:37 +0200

    Fix linting and init

--- qunicorn_core/util/config/smorest_config.py
+++ qunicorn_core/util/config/smorest_config.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 class SmorestProductionConfig:
     OPENAPI_VERSION = "3.0.2"
     OPENAPI_JSON_PATH = "api-spec.json"
